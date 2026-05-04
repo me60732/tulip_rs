a545ae746ce55ba98e42d9a13c4b280123763800
me60732 <me60732@gmail.com>
2026-05-04 16:55:28 +1000

    making way for new candle indicators

--- tulip_rs/src/candle_indicators/four_bar/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod concealingbabyswallow;
-pub mod bearishthreelinestrike;
-pub mod bullishthreelinestrike;
--- tulip_rs/src/candle_indicators/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod three_bar;
-pub use three_bar::*;
-pub mod one_bar;
-pub use one_bar::*;
-pub mod two_bar;
-pub use two_bar::*;
-pub mod four_bar;
-pub use four_bar::*;
