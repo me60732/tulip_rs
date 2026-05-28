88db11518d167caca66b79b130e6dd50944b70f3
me60732 <me60732@gmail.com>
2026-05-28 10:11:19 +1000

    Update lib.rs

--- tulip_rs/src/lib.rs
+++ tulip_rs/src/lib.rs
@@ -2,9 +2,6 @@
 #![feature(portable_simd)]
 
 pub mod candle_indicators;
-//pub mod candle_types;
-//pub mod cdlcommon;
-//pub mod cldcommontypes;
 pub mod common;
 pub mod indicator_types;
 pub mod indicators;
@@ -13,6 +10,4 @@ pub mod math;
 pub mod common_simd;
 pub mod ring_buffer;
 pub mod types;
-
-#[cfg(any(feature = "simd_options", feature = "simd_assets"))]
 pub mod math_simd;
