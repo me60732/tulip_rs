8b6677683eda8363cfd8b5b1507ea01fc0db16fa
me60732 <me60732@gmail.com>
2026-05-30 12:23:42 +1000

    removed dead code

--- tulip_rs/src/types.rs
+++ tulip_rs/src/types.rs
@@ -29,11 +29,6 @@ impl fmt::Display for IndicatorType {
         write!(f, "{:?}", self)
     }
 }
-/*#[derive(Debug)]
-pub struct Output {
-    pub indicators: Vec<Vec<f64>>,
-    pub state: IndicatorState,
-}*/
 
 #[derive(Clone, Copy)]
 pub struct Info {
@@ -73,48 +68,7 @@ pub struct DisplayGroup {
     pub display_type: DisplayType,        // Overlay | Indicator for this pane
     pub outputs: &'static [&'static str], // subset of optional_outputs that belong here
 }
-/*pub struct InfoIndicatorState<'a> {
-    pub array_values: Option<&'a [&'a str]>,
-    pub single_values: Option<&'a [&'a str]>,
-}
-impl<'a> InfoIndicatorState<'a> {
-    /// Returns the array values or an empty array if None.
-    pub fn get_array_values(&self) -> &[&'a str] {
-        self.array_values.unwrap_or(&[])
-    }
 
-    /// Returns the single values or an empty array if None.
-    pub fn get_single_values(&self) -> &[&'a str] {
-        self.single_values.unwrap_or(&[])
-    }
-}
-#[derive(Debug, Clone)]
-pub struct IndicatorState {
-    pub single_values: Option<Vec<f64>>,
-    pub array_values: Option<Vec<Vec<f64>>>,
-}*/
-
-/*pub trait IndicatorFromState {
-    fn indicator_from_state(
-        &mut self,
-        inputs: Vec<&Vec<f64>>,
-    ) -> Result<Vec<Vec<f64>>, IndicatorError>;
-}
-
-impl IndicatorState {
-    pub fn new(single_values: Option<Vec<f64>>, array_values: Option<Vec<Vec<f64>>>) -> Self {
-        Self {
-            single_values,
-            array_values,
-        }
-    }
-    pub fn single_values(&self) -> &[f64] {
-        self.single_values.as_deref().unwrap_or(&[])
-    }
-    pub fn array_values(&self) -> &[Vec<f64>] {
-        self.array_values.as_deref().unwrap_or(&[])
-    }
-}*/
 
 impl IndicatorError {
     pub fn message(&self) -> &str {
